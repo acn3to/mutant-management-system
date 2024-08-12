98c7f9a9e3935a84e7854f202884916936eaa768
acn3to <costanetoow@gmail.com>
2024-08-12 18:21:54 -0300

    initial commit

---
+++ src/test/java/com/codecrafters/devs/DevsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.codecrafters.devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
